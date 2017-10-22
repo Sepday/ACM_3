7a859a72785eba44feea489632614f0c5c8b8a4e
SeptDays <septdays@163.com>
2017-10-22 20:24:40 +0800

    W3.9UVA10340

--- W3.9/源.cpp
+++ W3.9/源.cpp
@@ -1,6 +1,36 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
+#define maxn 100000
+
+char s[maxn],t[maxn];
 int main()
 {
+	while ((scanf_s("%s", s,100001) != EOF)&&(scanf_s("%s", t,100001) != EOF))
+	{
+		bool a = false;
+		for (unsigned int i = 0; i < strlen(t); i++)
+		{
+			if (t[i] == s[0])
+			{
+				a = true;
+				for (unsigned int j = 0; j < strlen(s); j++)
+				{
+					if (s[j] != t[i + j])
+					{
+						a = false;
+					}
+				}
+			}
+		}
+		if (a)
+		{
+			cout << "Yes" << endl;
+		}
+		else
+		{
+			cout << "No" << endl;
+		}
+	}
 
 }
